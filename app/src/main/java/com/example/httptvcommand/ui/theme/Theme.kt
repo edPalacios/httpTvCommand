ff020d58fc7c2aa5329546936914b5b5e4389e05
eduardo.palacios <eduardo.palacios@tui.co.uk>
2021-06-30 19:29:23 +0100

    rename project name

--- app/src/main/java/com/example/httptvcommand/ui/theme/Theme.kt
+++ app/src/main/java/com/example/httptvcommand/ui/theme/Theme.kt
@@ -28,7 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun HttpTvCommandTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+fun RemoteHttpCommandTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
